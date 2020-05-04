f6c2206265c2980a772a937d14c10c1941d86df0
Luis Manuel Diaz <luismaprograma@gmail.com>
2020-05-03 22:59:52 -0500

    sitio de interes

--- app/src/main/java/com/example/guiavalledupar/MainActivity.java
+++ app/src/main/java/com/example/guiavalledupar/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.guiavalledupar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void SitiosInteres(View view) {
+        Intent intent = new Intent(this, Sitios.class);
+        startActivity(intent);
         android.widget.Toast toast1 =Toast.makeText(getApplicationContext(),"Sistios de interes", Toast.LENGTH_SHORT);
         toast1.show();
     }
