57f1e1bf2be131a0df640a85908ba0468281bae8
Luis Manuel Diaz <luismaprograma@gmail.com>
2020-05-04 23:18:55 -0500

    carrusel clicked

--- app/src/main/java/com/example/guiavalledupar/MainActivity.java
+++ app/src/main/java/com/example/guiavalledupar/MainActivity.java
@@ -7,26 +7,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends AppCompatActivity {
 
     CarouselView carouselView;
-
-    int[] sampleImages = {R.drawable.laduda, R.drawable.lajunta, R.drawable.callejonpurruputu, R.drawable.pueblobello, R.drawable.canaberales, R.drawable.lapaz};
+    TextView textInfoCarrusel;
+    int[] sampleImages = {R.drawable.laduda, R.drawable.lajunta, R.drawable.callejonpurruputu, R.drawable.pueblobello, R.drawable.canaberales, R.drawable.lapaz, R.drawable.colegioloperena};
+    String[] nameImg = {"LA DUDA", "lA JUNTA", "CALLEJON DE LA PURRUPUTU", "PUEBLO BELLO", "MANANTIAL DE CAÑAVERALES", "LA PAZ", "COLEGIO NACIONAL LOPERENA"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //textInfoCarrusel = findViewById(R.id.infocarrusel);
+
         carouselView = (CarouselView) findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
 
         carouselView.setImageListener(imageListener);
+
+        carouselView.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(MainActivity.this, "LA FOTO FUE TOMADA EN "+nameImg[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     ImageListener imageListener = new ImageListener() {
