8131688cd38bd1c729434bd8a7c96a537af81314
Luis Manuel Diaz <luismaprograma@gmail.com>
2020-06-11 22:52:20 -0500

    Consulta todos los restaurantes

--- app/src/main/java/com/example/guiavalledupar/Repository/RestauranteRepository.java
+++ app/src/main/java/com/example/guiavalledupar/Repository/RestauranteRepository.java
@@ -20,9 +20,11 @@ public class RestauranteRepository {
         ArrayList<Restaurante> restaurantes = new ArrayList<Restaurante>();
 
         switch (Posicion){
-            case 0:
-                restaurantes = GetChivoAsado();
-            break;
+            case 0: restaurantes = GetChivoAsado(); break;
+            case 1: restaurantes = GetResAsada(); break;
+            case 2: restaurantes = GetArrozCoco(); break;
+            case 3: restaurantes = GetSancocho(); break;
+            case 4: restaurantes = GetViudaPescado(); break;
         }
 
         return restaurantes;
@@ -35,6 +37,106 @@ public class RestauranteRepository {
         String HorarioRestaurante[] = Res.getStringArray(R.array.restaurante_horario_chivo_asado);
         String LatitudRestaurante[] = Res.getStringArray(R.array.restaurante_latitud_chivo_Asado);
         String LongitudRestaurante[] = Res.getStringArray(R.array.restaurante_longitud_chivo_Asado);
+
+        ArrayList<Restaurante> restaurantes = new ArrayList<Restaurante>();
+
+        for (int i = 0; i < NameRestaurante.length; i++){
+            Restaurante restaurante = new Restaurante();
+
+            restaurante.Name = NameRestaurante[i];
+            restaurante.Direccion = DireccionRestaurante[i];
+            restaurante.Horario = HorarioRestaurante[i];
+            restaurante.Longitud = Double.parseDouble(LongitudRestaurante[i]);
+            restaurante.Latitud = Double.parseDouble(LatitudRestaurante[i]);
+
+            restaurantes.add(restaurante);
+        }
+
+        return restaurantes;
+    }
+
+    public ArrayList<Restaurante> GetSancocho(){
+        Resources Res = this.context.getResources();
+        String NameRestaurante[] = Res.getStringArray(R.array.restaurante_name_sancocho);
+        String DireccionRestaurante[] = Res.getStringArray(R.array.restaurante_sancocho_direccion);
+        String HorarioRestaurante[] = Res.getStringArray(R.array.restaurante_horario_sancocho);
+        String LatitudRestaurante[] = Res.getStringArray(R.array.restaurante_latitud_sancocho);
+        String LongitudRestaurante[] = Res.getStringArray(R.array.restaurante_longitud_sancocho);
+
+        ArrayList<Restaurante> restaurantes = new ArrayList<Restaurante>();
+
+        for (int i = 0; i < NameRestaurante.length; i++){
+            Restaurante restaurante = new Restaurante();
+
+            restaurante.Name = NameRestaurante[i];
+            restaurante.Direccion = DireccionRestaurante[i];
+            restaurante.Horario = HorarioRestaurante[i];
+            restaurante.Longitud = Double.parseDouble(LongitudRestaurante[i]);
+            restaurante.Latitud = Double.parseDouble(LatitudRestaurante[i]);
+
+            restaurantes.add(restaurante);
+        }
+
+        return restaurantes;
+    }
+
+    public ArrayList<Restaurante> GetResAsada(){
+        Resources Res = this.context.getResources();
+        String NameRestaurante[] = Res.getStringArray(R.array.restaurante_name_carne_res_asada);
+        String DireccionRestaurante[] = Res.getStringArray(R.array.restaurante_sancocho_direccion);
+        String HorarioRestaurante[] = Res.getStringArray(R.array.restaurante_horario_carne_res_asado);
+        String LatitudRestaurante[] = Res.getStringArray(R.array.restaurante_latitud_carne_res_asado);
+        String LongitudRestaurante[] = Res.getStringArray(R.array.restaurante_longitud_carne_res_asado);
+
+        ArrayList<Restaurante> restaurantes = new ArrayList<Restaurante>();
+
+        for (int i = 0; i < NameRestaurante.length; i++){
+            Restaurante restaurante = new Restaurante();
+
+            restaurante.Name = NameRestaurante[i];
+            restaurante.Direccion = DireccionRestaurante[i];
+            restaurante.Horario = HorarioRestaurante[i];
+            restaurante.Longitud = Double.parseDouble(LongitudRestaurante[i]);
+            restaurante.Latitud = Double.parseDouble(LatitudRestaurante[i]);
+
+            restaurantes.add(restaurante);
+        }
+
+        return restaurantes;
+    }
+
+    public ArrayList<Restaurante> GetArrozCoco(){
+        Resources Res = this.context.getResources();
+        String NameRestaurante[] = Res.getStringArray(R.array.restaurante_name_arroz_coco);
+        String DireccionRestaurante[] = Res.getStringArray(R.array.restaurante_arroz_coco_direccion);
+        String HorarioRestaurante[] = Res.getStringArray(R.array.restaurante_horario_arroz_coco);
+        String LatitudRestaurante[] = Res.getStringArray(R.array.restaurante_latitud_arroz_coco);
+        String LongitudRestaurante[] = Res.getStringArray(R.array.restaurante_longitud_arroz_coco);
+
+        ArrayList<Restaurante> restaurantes = new ArrayList<Restaurante>();
+
+        for (int i = 0; i < NameRestaurante.length; i++){
+            Restaurante restaurante = new Restaurante();
+
+            restaurante.Name = NameRestaurante[i];
+            restaurante.Direccion = DireccionRestaurante[i];
+            restaurante.Horario = HorarioRestaurante[i];
+            restaurante.Longitud = Double.parseDouble(LongitudRestaurante[i]);
+            restaurante.Latitud = Double.parseDouble(LatitudRestaurante[i]);
+
+            restaurantes.add(restaurante);
+        }
+
+        return restaurantes;
+    }
+
+    public ArrayList<Restaurante> GetViudaPescado(){
+        Resources Res = this.context.getResources();
+        String NameRestaurante[] = Res.getStringArray(R.array.restaurante_name_viuda_pescado);
+        String DireccionRestaurante[] = Res.getStringArray(R.array.restaurant_viuda_pescado_direccion);
+        String HorarioRestaurante[] = Res.getStringArray(R.array.restaurante_horario_viuda_pescado);
+        String LatitudRestaurante[] = Res.getStringArray(R.array.restaurante_latitud_viuda_pescado);
+        String LongitudRestaurante[] = Res.getStringArray(R.array.restaurante_longitud_viuda_pescado);
 
         ArrayList<Restaurante> restaurantes = new ArrayList<Restaurante>();
 
